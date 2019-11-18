88fd16f1615c740be2ef36bd91f5e9f2449c1b15
Sean Dooley <sean.dooley.22@gmail.com>
2019-11-18 21:46:42 +0000

    Setup browser-sync and watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create(); // create a browser sync instance.
 
-gulp.task('browser-sync', function() {
+gulp.task('serve', function() {
     bs.init({
         server: {
             baseDir: "./"
         }
     });
+
+    gulp.watch('*.html').on('change', bs.reload);
+    gulp.watch('js/**/*.js').on('change', bs.reload);
 });
