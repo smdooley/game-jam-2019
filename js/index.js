b935be0283a8280a0afee78c1f5957d0f028ef40
smdooley <sean@mosquitodigital.co.uk>
2019-11-19 14:01:50 +0000

    Refactor to use Scene

--- js/index.js
+++ js/index.js
@@ -1,46 +1,73 @@
 var app = app || {};
 
-app.config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false,
-            gravity: {
-                y: 300
+window.onload = function() {
+    app.config = {
+        type: Phaser.CANVAS,
+        width: 640,
+        height: 320,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                debug: false,
+                gravity: {
+                    y: 300
+                }
             }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+        },
+        scene: Game
+    };
+
+    app.game = new Phaser.Game(app.config);
+
+    window.focus();
+}
+
+class Game extends Phaser.Scene{
+    constructor(){
+        super('Game');
     }
-};
 
-app.game = new Phaser.Game(app.config);
+    preload ()
+    {
+        this.load.image('player', 'assets/images/characters/platformChar_idle.png');
 
-function preload ()
-{
-    this.load.image('player', 'assets/images/characters/platformChar_idle.png');
+        this.load.image('ground', 'assets/images/tiles/platformPack_tile001.png');
+        this.load.image('board', 'assets/images/tiles/platformPack_tile066.png');
+        this.load.image('boardwalk', 'assets/images/tiles/platformPack_tile038.png');
+    }
 
-    this.load.image('ground', 'assets/images/tiles/platformPack_tile001.png');
-}
+    create ()
+    {
+        app.platforms = this.physics.add.staticGroup();
+        app.platforms.create(32 * 1, app.config.height - 32, 'ground');
+        app.platforms.create(32 * 3, app.config.height - 32, 'ground');
 
-function create ()
-{
-    app.player = this.physics.add.sprite(64, 300, 'player');
-    app.player.setCollideWorldBounds(true);
-    app.player.body.setGravityY(300);
+        app.boardwalks = this.physics.add.staticGroup();
+        app.boardwalks.create(32 * 5.5, app.config.height - 32, 'boardwalk');
+        app.boardwalks.create(32 * 7.5, app.config.height - 32, 'boardwalk');
 
-    app.platforms = this.physics.add.staticGroup();
-    app.platforms.create(64, 536, 'ground');
+        app.board = this.physics.add.sprite(0, 100, 'board');
+        app.board.setOrigin(0.5);
+        app.board.setCollideWorldBounds(true);
+        app.board.body.setGravityY(300);
 
-    this.physics.add.collider(app.player, app.platforms);
-}
+        // app.player = this.physics.add.sprite(64, 50, 'player');
+        // app.player.setCollideWorldBounds(true);
+        // app.player.body.setGravityY(300);
 
-function update ()
-{
+        // this.physics.add.collider(app.player, app.board);
+        // this.physics.add.collider(app.player, app.platforms);
+        this.physics.add.collider(app.board, app.platforms);
+        this.physics.add.collider(app.board, app.boardwalks);
+
+        this.input.on('pointerdown', this.launch, this);
+    }
+
+    update ()
+    {
+    }
+
+    launch() {
+        app.board.body.setVelocity(250,0);
+    }
 }
